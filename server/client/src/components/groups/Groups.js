d9614fbdce1039f620ebe1b69a2823b44a5ffbbd
Adir Sation <adirush120@gmail.com>
2020-12-19 15:37:16 +0200

    fix(Data): Removing mocked data

--- server/client/src/components/groups/Groups.js
+++ server/client/src/components/groups/Groups.js
@@ -11,57 +11,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Card from '@material-ui/core/Card';
 import ImageIcon from '@material-ui/icons/Image';
 import GroupDisplay from './GroupDisplay/GroupDisplay';
-import pic1 from '../../static/images/avatar/1.jpeg';
-import pic2 from '../../static/images/avatar/2.jpeg';
-import pic3 from '../../static/images/avatar/3.jpeg';
-import pic4 from '../../static/images/avatar/4.jpeg'
-
-const groupsfakedata = [
-    {
-        id: 1,
-        name: 'Wallak Pseder',
-        description: 'Wallak group',
-        playersData: [
-            { name: 'Adir Sation', picPath: pic1, id: 1, games: 3, side: 1, bullseye: 2, points: 10 },
-            { name: 'Shira Cohen', picPath: pic2, id: 2, games: 3, side: 1, bullseye: 2, points: 10 },
-            { name: 'Tomer Alony', picPath: pic3, id: 3, games: 3, side: 1, bullseye: 2, points: 10 },
-            { name: 'Yair Vered', picPath: pic4, id: 4, games: 3, side: 1, bullseye: 2, points: 10 }
-        ]
-    },
-    {
-        id: 2,
-        name: 'LoserTomer',
-        description: 'Tomer is the biggst loser',
-        playersData: [
-            { name: 'Adir Sation', picPath: pic1, id: 1, games: 3, side: 1, bullseye: 2, points: 10 },
-            { name: 'Shira Cohen', picPath: pic2, id: 2, games: 3, side: 1, bullseye: 2, points: 10 },
-            { name: 'Tomer Alony', picPath: pic3, id: 3, games: 3, side: 1, bullseye: 2, points: 10 },
-            { name: 'Yair Vered', picPath: pic4, id: 4, games: 3, side: 1, bullseye: 2, points: 10 }
-        ]
-    },
-    {
-        id: 3,
-        name: 'ShiraPira',
-        description: 'Yalla krayot',
-        playersData: [
-            { name: 'Adir Sation', picPath: pic1, id: 1, games: 3, side: 1, bullseye: 2, points: 7 },
-            { name: 'Shira Cohen', picPath: pic2, id: 2, games: 4, side: 3, bullseye: 4, points: 5 },
-            { name: 'Tomer Alony', picPath: pic3, id: 3, games: 5, side: 2, bullseye: 2, points: 5 },
-            { name: 'Yair Vered', picPath: pic4, id: 4, games: 6, side: 1, bullseye: 2, points: 10 }
-        ]
-    },
-    {
-        id: 4,
-        name: 'YakirMakir',
-        description: 'Ma kore yair ze yoav',
-        playersData: [
-            { name: 'Adir Sation', picPath: pic1, id: 1, games: 3, side: 1, bullseye: 2, points: 10 },
-            { name: 'Shira Cohen', picPath: pic2, id: 2, games: 3, side: 1, bullseye: 2, points: 10 },
-            { name: 'Tomer Alony', picPath: pic3, id: 3, games: 3, side: 1, bullseye: 2, points: 10 },
-            { name: 'Yair Vered', picPath: pic4, id: 4, games: 3, side: 1, bullseye: 2, points: 10 }
-        ]
-    },
-]
 
 const padding = 20;
 const useStyles = makeStyles((theme) =>
