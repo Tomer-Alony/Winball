593ec1bfb0a82923b4b78042786c94b04f25b7e8
Shiracohen3 <shushale333@gmail.com>
2020-12-21 23:04:27 +0200

    added style to games screen

--- server/client/src/components/games/Games.tsx
+++ server/client/src/components/games/Games.tsx
@@ -1,6 +1,8 @@
-import { makeStyles, Paper, WithStyles } from "@material-ui/core";
+import { makeStyles, Paper, TextField, WithStyles } from "@material-ui/core";
 import * as React from "react";
 import { Game } from '../../modles/Game';
+import FcbPic from '../../static/images/groups/Fcb.png'
+import RealPic from '../../static/images/groups/Rial.png'
 
 interface GameState {
 
@@ -20,6 +22,26 @@ const useStyles = makeStyles(theme => ({
     },
     gameContainer: {
         width: '100%',
+    },
+    details: {
+        flexDirection: 'row',
+        display: 'flex',
+        justifyContent: 'center',
+        marginTop: '15px',
+    },
+    pics: {
+        width: '60px',
+        height: '60px',
+        marginLeft: '22px',
+        marginRight: '22px'
+    },
+    guess: {
+        width: '35px',
+    },
+    guessContent: {
+        justifyContent: 'space-between',
+        display: 'contents',
+        maxWidth: '400px',
     }
 }));
 
@@ -33,8 +55,15 @@ export default function Games(props: GameProps, state: GameState) {
             <h1>Games</h1>
             <Paper className={classes.gameContainer}>
                 <div>16:30</div>
-                <div>
-                    <img src={require('../../static/images/groups/Fcb.png')}></img>
+                <div className={classes.details}> 
+                    <h4>FC Barcelona</h4>
+                    <img className={classes.pics} src={FcbPic}></img>
+                    <div className={classes.guessContent}>
+                        <TextField id="outlined-basic" className={classes.guess} variant="outlined" />
+                        <TextField id="outlined-basic" className={classes.guess} variant="outlined" />
+                    </div>
+                    <img className={classes.pics} src={RealPic}></img>
+                    <h4>Real Madrid</h4>
                 </div>
             </Paper>
         </div>
