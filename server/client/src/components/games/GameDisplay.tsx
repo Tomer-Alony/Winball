8731982407a332f67f4538e09b2e90ee8c7d2cf5
Shiracohen3 <shushale333@gmail.com>
2020-12-22 16:06:56 +0200

    added style of game display component

---
+++ server/client/src/components/games/GameDisplay.tsx
@@ -0,0 +1,80 @@
+import { makeStyles, Paper, TextField, Typography, WithStyles } from "@material-ui/core";
+import FcbPic from '../../static/images/groups/Fcb.png'
+import RealPic from '../../static/images/groups/Real.png'
+import * as React from "react";
+
+interface GameDataState {
+
+}
+
+interface GameDataProps {
+   
+}
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: '100%',
+        fontFamily: theme.typography.fontFamily,
+    },
+    container: {
+        maxHeight: '100%',
+    },
+    gameContainer: {
+        width: '100%',
+        paddingTop: '8px',
+        backgroundColor: '#00889138',
+        marginBottom: '5px'
+    },
+    details: {
+        flexDirection: 'row',
+        display: 'flex',
+        justifyContent: 'center',
+        marginTop: '10px',
+        paddingBottom: '15px',
+        alignItems: 'center'
+    },
+    pics: {
+        width: '60px',
+        height: '60px',
+        marginLeft: '22px',
+        marginRight: '22px'
+    },
+    guess: {
+        width: '35px',
+        borderColor: 'red'
+    },
+    guessContent: {
+        justifyContent: 'space-between',
+        display: 'flex',
+        minWidth: '200px',
+        alignItems: 'center'
+    },
+    input: {
+        backgroundColor: 'white',
+    }
+}));
+
+export default function GameDisplay(props: GameDataProps, state: GameDataState) {
+    const classes = useStyles();
+
+    const {  } = props;
+
+    return (
+        <>
+        <Paper className={classes.gameContainer}>
+            <div>16:30</div>
+            <div className={classes.details}> 
+                <Typography variant="body1" gutterBottom>FC Barcelona</Typography>
+                <img className={classes.pics} src={FcbPic}></img>
+                <div className={classes.guessContent}>
+                    <TextField id="outlined-basic" className={classes.guess} variant="outlined" InputProps={{className: classes.input }}/>
+                    <Typography variant="body2" gutterBottom>:</Typography>
+                    <TextField id="outlined-basic" className={classes.guess} variant="outlined" InputProps={{className: classes.input }}/>
+                </div>
+                <img className={classes.pics} src={RealPic}></img>
+                <Typography variant="body1" gutterBottom>Real Madrid</Typography>
+            </div>
+        </Paper>
+        </>
+    );
+};
--- server/client/src/components/games/Games.tsx
+++ server/client/src/components/games/Games.tsx
@@ -1,8 +1,8 @@
-import { makeStyles, Paper, TextField, WithStyles } from "@material-ui/core";
+import { makeStyles, Paper, TextField, Typography, WithStyles } from "@material-ui/core";
 import * as React from "react";
 import { Game } from '../../modles/Game';
-import FcbPic from '../../static/images/groups/Fcb.png'
-import RealPic from '../../static/images/groups/Rial.png'
+import GameDisplay from './GameDisplay';
+
 
 interface GameState {
 
@@ -20,29 +20,6 @@ const useStyles = makeStyles(theme => ({
     container: {
         maxHeight: '100%',
     },
-    gameContainer: {
-        width: '100%',
-    },
-    details: {
-        flexDirection: 'row',
-        display: 'flex',
-        justifyContent: 'center',
-        marginTop: '15px',
-    },
-    pics: {
-        width: '60px',
-        height: '60px',
-        marginLeft: '22px',
-        marginRight: '22px'
-    },
-    guess: {
-        width: '35px',
-    },
-    guessContent: {
-        justifyContent: 'space-between',
-        display: 'contents',
-        maxWidth: '400px',
-    }
 }));
 
 export default function Games(props: GameProps, state: GameState) {
@@ -53,19 +30,9 @@ export default function Games(props: GameProps, state: GameState) {
     return (
         <div style={{ textAlign: 'center' }} className={classes.root}>
             <h1>Games</h1>
-            <Paper className={classes.gameContainer}>
-                <div>16:30</div>
-                <div className={classes.details}> 
-                    <h4>FC Barcelona</h4>
-                    <img className={classes.pics} src={FcbPic}></img>
-                    <div className={classes.guessContent}>
-                        <TextField id="outlined-basic" className={classes.guess} variant="outlined" />
-                        <TextField id="outlined-basic" className={classes.guess} variant="outlined" />
-                    </div>
-                    <img className={classes.pics} src={RealPic}></img>
-                    <h4>Real Madrid</h4>
-                </div>
-            </Paper>
+            <GameDisplay></GameDisplay>
+            <GameDisplay></GameDisplay>
+            <GameDisplay></GameDisplay>
         </div>
     );
 };
